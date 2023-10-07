b373c88472703acec51957355d812379b3fbccc4
GhostBang <rezidreta@gmail.com>
2023-10-07 12:40:13 +0500

    commit

---
+++ bin/kt1.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+
+void main(List<String> arguments) async{
+  Dio client = Dio();
+  String url = 'https://dummyjson.com/products';
+  Response response = await client.get(url);
+
+  Map<String, dynamic> data = response.data;
+  List<dynamic> product = data['products'];
+
+  int count=0;
+  for (var el in product) 
+  
+  {
+    if(el['brand']=='Apple'){
+      count++;
+    }
+  }
+  
+  
+  print(count);
+
+}
